696e04f2aaaf177a42ec48fb747a4facd3c12979
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2011-12-31 19:39:48 +0100

    fixit
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- src/system.py
+++ src/system.py
@@ -1,10 +1,9 @@
 from run import bind,put,putf,c
-from src.irc import irc_send
 import os
 
 bind("uname","src_system","pub","$uname")
 
 def uname(nick,host,chan,arg):
-	irc_send(chan, ' '.join(os.uname))
+	put("PRIVMSG %s :%s" % (chan, ' '.join(os.uname)))
 
 import run
