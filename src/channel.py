85d27e1ed1480cce5b85db8d1da4a12e5160e4ad
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-01-03 17:38:30 +0100

    try
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- src/channel.py
+++ src/channel.py
@@ -338,8 +338,8 @@ def channel_listuser(nick,host,chan,arg):
 	for owner in _chandb.execute("select auth from channel where channel='%s' and flags='n'" % chan):
 		put("NOTICE %s :[%s] %s is the owner" % (nick,chan,owner[0]))
 	op = _chandb.execute("select auth from channel where channel='%s' and flags='o'" % chan)
-	for operators in op.fetchall():
-		put("NOTICE %s :[%s] Operators: %s" % (nick,chan,operators))
+	operators = op.fetchall()
+	put("NOTICE %s :[%s] Operators: %s" % (nick,chan,operators))
 	for voice in _chandb.execute("select auth from channel where channel='%s' and flags='v'" % chan):
 		put("NOTICE %s :[%s] %s is a voice" % (nick,chan,voice[0]))
 
