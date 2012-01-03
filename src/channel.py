dda92246ff75f0c2774b4f5edb13991365ef1ab4
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-01-03 17:41:44 +0100

    try again
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- src/channel.py
+++ src/channel.py
@@ -337,7 +337,7 @@ def channel_delvoice(nick,host,chan,arg):
 def channel_listuser(nick,host,chan,arg):
 	for owner in _chandb.execute("select auth from channel where channel='%s' and flags='n'" % chan):
 		put("NOTICE %s :[%s] %s is the owner" % (nick,chan,owner[0]))
-	operators = ', '.join(_chandb.execute("select auth from channel where channel='%s' and flags='o'" % chan).fetchall())
+	operators = ', '.join((_chandb.execute("select auth from channel where channel='%s' and flags='o'" % chan)).fetchall())
 	put("NOTICE %s :[%s] Operators: %s" % (nick,chan,operators))
 	for voice in _chandb.execute("select auth from channel where channel='%s' and flags='v'" % chan):
 		put("NOTICE %s :[%s] %s is a voice" % (nick,chan,voice[0]))
