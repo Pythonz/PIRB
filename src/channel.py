2b1fb315e77de18e69c580ec892c9572975ca3e6
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-01-06 17:11:06 +0100

    try
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- src/channel.py
+++ src/channel.py
@@ -337,13 +337,11 @@ def channel_delvoice(nick,host,chan,arg):
 def channel_listuser(nick,host,chan,arg):
 	for owner in _chandb.execute("select auth from channel where channel='%s' and flags='n'" % chan):
 		put("NOTICE %s :[%s] %s is the owner" % (nick,chan,owner[0]))
-	_chandb.execute("select auth from channel where channel='%s' and flags='o'" % chan)
-	operators = ' '.join(_chandb)
-	#operators = ' '.join(set(str(op.fetchall())))
+	op = _chandb.execute("select auth from channel where channel='%s' and flags='o'" % chan)
+	operators = ' '.join(set(str(op.fetchall())))
 	put("NOTICE %s :[%s] Operators: %s" % (nick,chan,operators))
-	_chandb.execute("select auth from channel where channel='%s' and flags='v'" % chan)
-	#voices = ' '.join(set(str(v.fetchall())))
-	voices = ' '.join(_chandb)
+	v = _chandb.execute("select auth from channel where channel='%s' and flags='v'" % chan)
+	voices = ' '.join(set(str(v.fetchall())))
 	put("NOTICE %s :[%s] %s is a voice" % (nick,chan,voices))
 
 def channel_join(text):
