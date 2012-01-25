d316c60e023ce25f75c3a39de74faf3c16de5e67
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-01-25 14:45:30 +0100

    [59] added WHOX support
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- src/user.py
+++ src/user.py
@@ -3,7 +3,8 @@ from run import put,putf,bind,c,printa,printc,printe
 bind("nick_in_use","src_user","raw","433")
 bind("on_quit","src_user","raw","QUIT")
 bind("whois_330","src_user","raw","330")
-bind("check_330","src_user","raw","352")
+bind("who_354", "src_user", "raw", "354")
+bind("check_user","src_user","raw","352")
 bind("on_366","src_user","raw","366")
 bind("on_nickchange","src_user","raw","NICK")
 bind("whois_307","src_user","raw","307")
@@ -27,11 +28,22 @@ def whois_307(text):
 	_userdb.execute("delete from auth where nick='%s'" % text.split()[3])
 	_userdb.execute("insert into auth values ('%s','%s')" % (text.split()[3], text.split()[3]))
 
-def check_330(text):
-	put("WHOIS %s" % text.split()[7])
+def who_354(text):
+	if text.split()[3] == "111":
+		_userdb.execute("delete from auth where nick='%s'" % text.split()[4])
+		_userdb.execute("insert into auth values ('%s','%s')" % (text.split()[4], text.split()[5]))
+
+def check_user(text):
+	if c.get("SERVER", "address").lower().endswith(".quakenet.org"):
+		put("WHO %s %nat,111" % text.split()[7])
+	else:
+		put("WHOIS %s" % text.split()[7])
 
 def on_366(text):
-	put("WHO %s" % text.split()[3])
+	if c.get("SERVER", "address").lower().endswith(".quakenet.org"):
+		put("WHO %s %nat,111" % text.split()[3])
+	else:
+		put("WHO %s" % text.split()[3])
 
 def on_nickchange(text):
 	for data in _cache.execute("select name from botnick"):
@@ -41,10 +53,16 @@ def on_nickchange(text):
 		_cache.execute("update botnick set name='%s'" % _botnick)
 		put("NICK %s" % _botnick)
 	_userdb.execute("delete from auth where nick='%s'" % text.split()[0][1:].split("!")[0])
-	put("WHOIS %s" % text.split()[2])
+	if c.get("SERVER", "address").lower().endswith(".quakenet.org"):
+		put("WHO %s %nat,111" % text.split()[2])
+	else:
+		put("WHOIS %s" % text.split()[2])
 
 def getauth(nick):
-	putf("WHOIS %s" % nick)
+	if c.get("SERVER", "address").lower().endswith(".quakenet.org"):
+		putf("WHO %s %nat,111" % nick)
+	else:
+		putf("WHOIS %s" % nick)
 	for data in _userdb.execute("select auth from auth where nick='%s'" % nick):
 		return data[0]
 
