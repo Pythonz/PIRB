902baea53285481bb04d80d55986d6d9c3e7207a
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-01-30 16:38:40 +0100

    [77] :/
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- src/ctcp.py
+++ src/ctcp.py
@@ -1,6 +1,6 @@
 from run import bind,put,putf,printa,printc,printe
 
-bind("ping","src_ctcp","msg","PING")
+bind("ping","src_ctcp","msg","\001PING")
 
 def ctcp(target,text):
 	put("PRIVMSG %s :%s" % (target,text))
