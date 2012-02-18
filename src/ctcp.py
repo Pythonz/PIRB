86f8d443754ad13291de31fb6cba0771d325366f
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-18 18:29:38 +0100

    adding important ctcps, filling it out later
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- src/ctcp.py
+++ src/ctcp.py
@@ -2,11 +2,23 @@ from pirb import bind,put
 
 def load():
 	bind("ping","src_ctcp","msg",u"\u0001PING\u0001")
+	bind("version","src_ctcp","msg",u"\u0001VERSION\u0001")
+	bind("userinfo","src_ctcp","msg",u"\u0001USERINFO\u0001")
+	bind("finger", "src_ctcp","msg",u"\u0001FINGER\u0001")
 
 def ctcp(target,text):
 	put(u"PRIVMSG %s :\u0001%s\u0001" % (target,text))
 
 def ping(nick,uhost,arg):
-	ctcp(nick, "PING")
+	ctcp(nick, "PONG")
+
+def version(nick,uhost,arg):
+	ctcp(nick, "This is my answer")
+
+def userinfo(nick,uhost,arg):
+	ctcp(nick, "This is my answer")
+
+def finger(nick,uhost,arg):
+	ctcp(nick, "This is my answer")
 
 import pirb
