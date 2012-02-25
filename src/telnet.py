ba68743ed20fd03415b2231b193bcde69ee9acb2
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-25 23:13:22 +0100

    do it so
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- src/telnet.py
+++ src/telnet.py
@@ -58,7 +58,7 @@ class Telnet:
 						sha.update(data[6:])
 						self.send(sock,"Hash: %s" % str(sha.hexdigest()))
 					elif cmd == "put":
-						putf(data[4:])
+						put(data[4:])
 						self.send(sock,"Sent: %s" % data[4:])
 					elif cmd == "restart":
 						if os.access("pirb.pid", os.F_OK):
